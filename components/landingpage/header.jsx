a307a2b2950005bcf054908260c0852ef075730b
ayanSCYY <ayanm3206@gmail.com>
2024-06-30 22:21:06 +0530

    feat: youtube clone

---
+++ components/landingpage/header.jsx
@@ -0,0 +1,18 @@
+export const Header = () => {
+    return (    
+        <div className="flex justify-between">
+            <div className="flex justify-evenly">
+                <div></div>
+                <div></div>
+            </div>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
